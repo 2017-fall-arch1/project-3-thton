3af3800a9dd2bed2207d42483aa6c9ff1b064708
Toan Ton <thton@miners.utep.edu>
2017-12-05 09:18:25 -0700

    Create buzzer.c

---
+++ lab3/buzzer.c
@@ -0,0 +1,24 @@
+#include <msp430.h>
+#include <buzzer.h>
+
+void buzzer_init(){
+
+  /*
+    Direct timer A output "TA0.1" to P2.6.
+    According to table 21 from data sheet:
+      P2SEL2.6, P2SEL2.7, anmd P2SEL.7 must be zero
+      P2SEL.6 must be 1
+    Also: P2.6 direction must be output
+   */
+  
+  timerAUpmode();            /* used to drive speaker*/
+  P2SEL2 &= ~(BIT6 | BIT7);
+  P2SEL &= ~BIT7;
+  P2SEL |= BIT6;
+  P2DIR = BIT6;              /* enable output to speaker */
+}
+
+void buzzer_set_note(short note){
+  CCR0 = note;
+  CCR1 = note >> 1;          /* onw half cycle */
+}
